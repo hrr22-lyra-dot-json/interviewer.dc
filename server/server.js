d12c888e7a0dcb96110e79ebb339182041227d7f
Kyle Anson <kyle.anson7@gmail.com>
2017-04-08 19:57:02 -0400

    Add request-handler file connection

--- server/server.js
+++ server/server.js
@@ -5,11 +5,14 @@ var path = require('path');
 
 const app = express();
 
+// Middleware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../')));
 
+// Routes
+require('./routes.js')(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port);
