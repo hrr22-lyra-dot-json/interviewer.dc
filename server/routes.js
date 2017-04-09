42752b485f7b6a05899acf9ec0c11fb8b4c75c2c
Kyle Anson <kyle.anson7@gmail.com>
2017-04-08 20:05:43 -0400

    Added routes file with routes that will need to exist eventually Only post to user is uncommented for the time being

---
+++ server/routes.js
@@ -0,0 +1,16 @@
+var requestHandler = require('./request-handler.js');
+
+module.exports = function(app) {
+  //app.post('/api/Meeting', requestHandler.addMeeting);
+  //app.delete('/api/Meeting', requestHandler.deleteMeeting);
+  app.post('/api/User', requestHandler.addUser);
+  //app.post('/api/UserMeeting', requestHandler.addUserMeeting);
+  // List all rows in the UserMeeting table
+  //app.get('/api/allUserMeeting', requestHandler.listUserMeeting);
+  // List all meetings that a specific user is part of
+  //app.get('/api/UserMeeting', requestHandler.listUsersMeetings);
+  //app.delete('/api/UserMeeting', requestHandler.deleteUserMeeting);
+
+  // Assume link is a meeting link
+  //app.get('/*', requestHandler.findMeeting);
+};
