28f1a50de9c545a269c8aa533ee5903a113034d4
Kyle Anson <kyle.anson7@gmail.com>
2017-04-10 14:39:12 -0400

    Added error handling to current request handlers Not entirely sure if .catch works on a .spread operation, should look into that later

--- server/request-handler.js
+++ server/request-handler.js
@@ -13,6 +13,7 @@ exports.addMeeting = function(req, res) {
 ** Expected request body: {username(string): 'username', email(string): 'user email'}
 ** Expected response if user exists: 409 Conflict status
 ** Expected response if user does not exist: 201 Created status
+** Expected response on database error: 500 Internal Server Error status
 */
 exports.addUser = function(req, res) {
   // See if username or email already exists in database
@@ -33,6 +34,9 @@ exports.addUser = function(req, res) {
         email: req.body.email
       }).then(function(User) {
         res.status(201).send();
+      }).catch(function(err) {
+        console.error(err);
+        res.status(500).send();
       });
     }
   });
@@ -42,6 +46,7 @@ exports.addUser = function(req, res) {
 ** Expected request body: {user_id(integer): 'user id', meeting_id(integer): 'meeting id'}
 ** Expected response if usermeeting exists: 409 Conflict status
 ** Expected response if usermeeting does not exist: 201 Created status
+** Expected response on database error: 500 Internal Server Error status
 */
 exports.addUserMeeting = function(req, res) {
   UserMeeting.findOrCreate({where: {user_id: req.body.user_id, meeting_id: req.body.meeting_id}})
@@ -51,14 +56,21 @@ exports.addUserMeeting = function(req, res) {
     } else {
       res.status(409).send();
     }
+  }).catch(function(err) {
+    console.error(err);
+    res.status(500).send();
   });
 };
 
 /*
 ** Expected response: 200 OK status, {usermeetings(array): [{user_id(integer): 'user id', meeting_id(integer): 'meeting id'}]}
+** Expected response on database error: 500 Internal Server Error status
 */
 exports.listUserMeetings = function(req, res) {
   UserMeeting.findAll().then(function(UserMeetings) {
     res.status(200).send(UserMeetings);
+  }).catch(function(err) {
+    console.error(err);
+    res.status(500).send();
   });
 };
