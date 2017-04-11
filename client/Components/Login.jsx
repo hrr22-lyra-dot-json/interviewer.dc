a643d3583b1b7793eeb7f9f4b64d60981813d74b
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-04-11 17:44:25 -0400

    Refactor login page with Materialize CSS syntax

--- client/Components/Login.jsx
+++ client/Components/Login.jsx
@@ -10,23 +10,12 @@ export class Login extends React.Component {
 
     return (
       <div className="container login">
-        <div className="jumbotron">
-          <div className="row align-items-center justify-contents-center">
-            <div className="col">
-              <h1>Interviewer.DC</h1>
-            </div>
-            <div className="w-100"></div>
-            <div className="col">
-              <p>Brought to you by Project Washington</p>
-            </div>
-            <div className="w-100"></div>
-            <div className="col">
-              <img src="client/assets/capitol-bldg.png" alt="Interviewer.DS Logatron" />
-            </div>
-            <div className="w-100"></div>
-            <div className="col">
-              <button className="btn btn-default" onClick={auth.login.bind(this)}>Login</button>
-            </div>
+        <div className="row align-items-center justify-contents-center">
+          <div className="col s12">
+            <img src="client/assets/capitol-bldg.png" alt="Interviewer.DS Logatron" />
+          </div>
+          <div className="col s12">
+            <button className="btn btn-default" onClick={auth.login.bind(this)}>Login</button>
           </div>
         </div>
       </div>
