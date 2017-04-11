ae8631cfea0bd5e96831b5f07d2378c70b94790d
Kyle Anson <kyle.anson7@gmail.com>
2017-04-11 14:32:37 -0400

    Fixed incorrect association user_id -> owner_id

--- server/database/models/meeting.js
+++ server/database/models/meeting.js
@@ -8,7 +8,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        Meeting.belongsTo(models.User, {foreignKey: 'user_id'});
+        Meeting.belongsTo(models.User, {foreignKey: 'owner_id'});
       }
     }
   });
