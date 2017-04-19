1a3cfe45da904f807b6a8db5aff570bb43de4d4b
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-04-18 23:42:08 -0400

    Move logout link into nav button; remove nav title; revert login page; add footer

--- client/Components/Login.jsx
+++ client/Components/Login.jsx
@@ -12,26 +12,29 @@ export class Login extends React.Component {
 
     return (
       <div className="splash">
+
         <nav className="splash-nav blue darken-3">
           <div className="nav-wrapper">
             <a href="#" className="brand-logo center">Interviewer Direct Connection</a>
-            <a href="/auth/google">Login with Google</a>
+            {/* <a href="/auth/google">Login with Google</a>
 
-            <ul className="right"><li><button onClick={hell.isIn.bind(hell)} className="right" className="btn waves-effect waves-light indigo darken-4">Sign In</button></li></ul>
+            <ul className="right"><li><button onClick={hell.isIn.bind(hell)} className="right" className="btn waves-effect waves-light indigo darken-4">Sign In</button></li></ul> */}
+            <ul className="right"><li><a href="/auth/google" className="right" className="btn waves-effect waves-light indigo darken-4">Sign In</a></li></ul>
+
           </div>
         </nav>
 
-        <div className="parallax-container">
-          <div className="parallax"><img src="client/assets/man-writing.jpg" id="splash-img-top" alt="Interviewer.DS Logatron" /></div>
+        <div className="row splash-header-row center-align grey lighten-2">
+          <img src="client/assets/splash-header.jpg" className="responsive-img splash-header" alt="Interviewer.DS Logatron" />
         </div>
 
-        <div className="section white login center-align">
-          <div className="row container">
+        <div className="container login center-align">
+          <div className="row">
             <h3 className="header">The Next Interviewing Platform Has Arrived</h3>
             <div className="col s12 m4">
               <span className="glyphicons glyphicons-facetime-video x4 splash-icons"></span>
               <h5>Streamlined Video Interface</h5>
-              <p>Conduct interviews in a carefully-designed web platform full of features such as recording, white boarding, and code sharing. Early arrivals will also be greeted by a waiting page in the room lobby until the interviewer begins the meeting.</p>
+              <p>Conduct interviews in a carefully-designed web platform, with features such as recording, white boarding, and code sharing. Early arrivals will remain in the room lobby until the interviewer begins the meeting.</p>
             </div>
             <div className="col s6 m4">
               <span className="glyphicons glyphicons-calendar x4 splash-icons"></span>
@@ -43,12 +46,12 @@ export class Login extends React.Component {
               <h5>Email Notification System</h5>
               <p>Send email notifications when meetings are created, updated, or deleted. Email integration allows for complete oversight of meetings for both the interviewer and the interviewee, reducing no-shows and miscommunication.</p>
             </div>
-
+          </div>
+          <div className="row center-align splash-try-button">
+            <a href="/auth/google" className="right" className="btn waves-effect waves-light indigo darken-4">Try It Out!</a>
           </div>
         </div>
-        <div className="parallax-container">
-          <div className="parallax"><img src="client/assets/laptop-and-notepad.jpg" id="splash-img-bottom" alt="Laptop and notepad on desk" /></div>
-        </div>
+
       </div>
     )
   }
