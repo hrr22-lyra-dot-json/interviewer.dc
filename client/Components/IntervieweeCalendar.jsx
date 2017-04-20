bb5a6622b79a1be79fd7ac1fc729491879940bb5
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-04-20 15:06:19 -0400

    Refactor modal positioning

--- client/Components/IntervieweeCalendar.jsx
+++ client/Components/IntervieweeCalendar.jsx
@@ -19,12 +19,6 @@ const customStyles = {
     zIndex: 10
   },
   content : {
-    top                   : '20%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
   }
 };
 
@@ -172,7 +166,7 @@ class CalendarInterviewee extends React.Component {
           </div>
 
           <CalView events={this.state.eventsAndSlots} selectable={this.state.selectable}  selectSlot={this.addInfo.bind(this)} eventClick={this.eventClick.bind(this)} />
-            <div>
+            <div className="container">
             <Modal
               isOpen={this.state.modalIsOpen}
               onAfterOpen={this.afterOpenModal}
