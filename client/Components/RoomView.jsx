e5e631afac2e773d7887fb2a12e4b396af3cdcc0
Simon de Moor <simondemoor0@gmail.com>
2017-04-20 11:10:52 -0700

    Update RoomView.jsx

--- client/Components/RoomView.jsx
+++ client/Components/RoomView.jsx
@@ -124,6 +124,7 @@ class RoomView extends React.Component {
                               <a   >{interview.start}</a>
                                 <a   >{interview.interviewee_name}</a>
                                 <a   >{interview.interviewee_email}</a>
+                                <a href={'https://drive.google.com/drive/folders/' + interview.drive_link}>Link to Google drive folder</a>
                                 <Link to={{ pathname: '/interviewroom', state: interview.id + '$' + roomDatabaseId/*, query: {roomname: room.job_position + room.owner_id}*/ }} >
                                   <span className="glyphicons glyphicons-exit rooms-section-icons"></span>
                                 </Link>
