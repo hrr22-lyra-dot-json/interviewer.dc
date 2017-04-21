27cde0381963153c068d41517bce74669a98e314
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-04-20 23:20:35 -0400

    Add Trace config file

---
+++ trace.config.js
@@ -0,0 +1,5 @@
+// trace.config.js
+module.exports = {
+  serviceName: 'Frontend API',
+  apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4Zjk1OTRhYzM4NjA0MzI4Y2UzZWJiZiIsImlhdCI6MTQ5MjczNjMzMH0.PJxBVI4mXGoUOOH-6tNZJo3UGedkinjprEo2nLDYmEc'
+};
