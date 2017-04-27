152587cd95cd9cf34771cb1aac18dea5ae3523ab
Jason Kim <kasonjim@gmail.com>
2017-04-26 17:03:37 -0700

    REMOVE OLD COMMENTS/CODES FROM QuestionService.js

--- client/Services/InterviewService.js
+++ client/Services/InterviewService.js
@@ -1,47 +1,32 @@
-import { EventEmitter } from 'events'
-import axios from 'axios'
+import { EventEmitter } from 'events';
+import axios from 'axios';
 
 export default class InterviewService extends EventEmitter {
   constructor() {
-    super()
+    super();
   }
 
-  // addOne(question) {
-  //   this.addAQuestion(question, this.getThem.bind(this))
-  // }
   getThem(roomDbId) {
-    this.getInterviews(roomDbId, this.gotInterviews.bind(this))
+    this.getInterviews(roomDbId, this.gotInterviews.bind(this));
   }
   getOne(interviewId) {
-    this.getInterview(interviewId, this.gotInterview.bind(this))
+    this.getInterview(interviewId, this.gotInterview.bind(this));
 
   }
 
-  // addAQuestion(question, callback) {
-  //   axios.post('/api/Interview', question//meeting_id:roomDbId, question: question
-  //   )
-  //   .then(function(response) {
-  //     callback(question.meeting_id)
-  //   })
-  // }
   getInterview(id, callback){
     axios.get('/api/Interviews', {
       params:{
         id: id
       }
     }).then(function(response) {
-      console.log('getinterviewres', response)
-      callback(response.data[0])
+      // console.log('getinterviewres', response)
+      callback(response.data[0]);
     })
     .catch(function (error) {
-      console.log('get interview error', error);
+      // console.log('get interview error', error);
     });
   }
-
-  // {
-  //       roomid: roomDbId,
-  //       owner_id: userid
-  //     }
 
   getInterviews(params, callback) {
     var userid = JSON.parse(localStorage.getItem('googleUser')).user.id;
@@ -50,16 +35,16 @@ export default class InterviewService extends EventEmitter {
       params: params
     })
     .then(function(response) {
-      callback(response.data)
+      callback(response.data);
     })
     .catch(function (error) {
-      console.log('get interviews error', error);
+      // console.log('get interviews error', error);
     });
   }
   gotInterviews(interviews) {
-    this.emit('got_interviews', interviews)
+    this.emit('got_interviews', interviews);
   }
   gotInterview(interview) {
-    this.emit('got_interview', interview)
+    this.emit('got_interview', interview);
   }
 }
